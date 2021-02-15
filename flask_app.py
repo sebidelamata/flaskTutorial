d3f858017b6add2cf356f4d8651d8a4fd0816a9f
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-02-15 15:55:38 -0500

    added first lines of actual code for the app

--- flask_app.py
+++ flask_app.py
@@ -1,1 +1,15 @@
-print("hello world")
+# this script follows a tutorial at
+# http://www.discoversdk.com/blog/getting-started-with-flask?utm_medium=blog&utm_campaign=aaron&utm_source=quora
+# on creating a basic web app with flask
+
+# importing libraries
+import flask
+
+# here we are creating an instance of the flask class and
+# pass the name of the current module to its constructor
+app = flask.Flask(__name__)
+
+
+# next we have a straight up python function that returns a super cheesy string
+def home():
+    return "Welcome to my home page that I made available with the help of flask."
