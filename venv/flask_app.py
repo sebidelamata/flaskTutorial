222e54767b67b360d7d822ca9735db88aaff19e9
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-02-16 15:49:09 -0500

    created taskmaster sql branch as this is a major change to the app

--- venv/flask_app.py
+++ venv/flask_app.py
@@ -3,11 +3,28 @@
 # on creating a basic web app with flask
 
 # importing libraries
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 # here we are creating an instance of the flask class and
 # pass the name of the current module to its constructor
 app = Flask(__name__)
+
+# here we are configuring a resource (our sql database)
+#### SIDEQUEST ALERT ####
+# I didn't know what a URI was so looked it up, but the answer
+# I got made me think this should be labeled a url and not uri
+# I mean the database is a resource and it is implied that you are using http to access it right?
+# here is what i found:
+#
+# The terms “URI” and “URL” are often used interchangeably, but they are not exactly the same.
+# A URI is an identifier of a specific resource. Like a page, or book, or a document.
+# A URL is special type of identifier that also tells you how to access it, such as
+# HTTPs, FTP, etc.—like https://www.google.com.
+# If the protocol (https, ftp, etc.) is either present or implied for a domain,
+# you should call it a URL—even though it’s also a URI.
+# see what I mean? Anyways...
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://test.db'
 
 
 # this function will define our home page, but we need a way to know this is our home page
